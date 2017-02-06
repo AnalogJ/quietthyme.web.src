e109fc9df693d87bce15c7b11c4cea4be9f4bdfa
Jason Kulatunga <jk17@ualberta.ca>
2017-02-05 22:36:20 -0800

    update app modules to support angular login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AccountLoginComponent } from './account-login/account-login.component';
+import { AccountRegisterComponent } from './account-register/account-register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountLoginComponent,
+    AccountRegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    RouterModule.forRoot([
+      { path: 'login', component: AccountLoginComponent },
+      { path: 'register', component: AccountRegisterComponent },
+
+
+      { path: 'dashboard', component: DashboardComponent }, //, canActivate: [AuthGuard] },
+      // { path: 'project/create', component: ProjectCreateComponent, canActivate: [AuthGuard] },
+      // { path: 'project/:serviceType/:orgId/:repoId/edit', component: ProjectEditComponent, canActivate: [AuthGuard] },
+      // { path: 'project/:serviceType/:orgId/:repoId/pullrequests/:prNumber', component: ProjectDeployComponent, canActivate: [AuthGuard] },
+      // { path: 'project/:serviceType/:orgId/:repoId/pullrequests/:prNumber/logs', component: ProjectDeployLogsComponent, canActivate: [AuthGuard] },
+
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      // { path: '**', component: PageNotFoundComponent }
+      { path: '**', redirectTo: 'login' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
