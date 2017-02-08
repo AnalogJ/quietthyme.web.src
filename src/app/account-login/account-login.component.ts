c7031d37c95781b7123f9136a22d061a8636fd77
Jason Kulatunga <jk17@ualberta.ca>
2017-02-08 11:59:10 -0800

    forgot missing components.

---
+++ src/app/account-login/account-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-login',
+  templateUrl: './account-login.component.html',
+  styleUrls: ['./account-login.component.less']
+})
+export class AccountLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/account-register/account-register.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AccountRegisterComponent } from './account-register.component';
+
+describe('Component: AccountRegister', () => {
+  it('should create an instance', () => {
+    let component = new AccountRegisterComponent();
+    expect(component).toBeTruthy();
+  });
+});
