015e84f613017b1d51a7d17265a219adff71ee4a
Jason Kulatunga <jk17@ualberta.ca>
2017-02-11 13:20:46 -0800

    fix endpoint./

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,6 +1,6 @@
 import { environment } from '../environments/environment'
 
 export class AppSettings {
-    public static get API_ENDPOINT(): string { return 'https://api.capsulecd.com/' + environment.apiVersion; }
+    public static get API_ENDPOINT(): string { return 'https://api.quietthyme.com/' + environment.apiVersion; }
 
 }
