c2741078afa251bf22d0916cf43fa12bf18c97cc
Jason Kulatunga <jk17@ualberta.ca>
2017-03-03 00:06:17 -0800

    added page title variables. deleting unnecessary css. update template color.

---
+++ src/app/models/page-title-breadcrumb.ts
@@ -0,0 +1,5 @@
+export class PageTitleBreadcrumb {
+    active: boolean
+    name: string
+    path: string
+}
--- src/app/partials/page-title/page-title.component.ts
+++ src/app/partials/page-title/page-title.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {PageTitleBreadcrumb} from 'app/models/page-title-breadcrumb'
 @Component({
   selector: 'quietthyme-page-title',
   templateUrl: './page-title.component.html',
   styleUrls: ['./page-title.component.less']
 })
 export class PageTitleComponent implements OnInit {
+
+  @Input() title : string;
+  @Input() subtitle : string;
+  @Input() breadcrumbs : Array<PageTitleBreadcrumb> = []
+
 
   constructor() { }
 
