7e48bb67390f4799f4717649d9b58a1422fa9afd
Jason Kulatunga <jk17@ualberta.ca>
2017-08-16 19:19:54 -0700

    fix links to use routerLink inside app. Added content blurb for all storage types. Added link to library and display Blackhole text.

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -13,4 +13,38 @@ export class AppSettings {
     public static get SUPPORTED_STORAGE_TYPES(): string[] {
         return AppSettings.KLOUDLESS_STORAGE_TYPES.concat(['quietthyme'])
     }
+
+    public static get STORAGE_DETAILS(): any {
+        return {
+            'quietthyme': {
+                'display_name': 'QuietThyme',
+                'details': 'This storage type is not available during beta.',
+                'blackhole_path': null
+
+            },
+            'box': {
+                'display_name': 'Box',
+                'details': 'Manage content across your business; from simple file sharing to building custom apps.',
+                'blackhole_path': 'QuietThyme/blackhole'
+            },
+            'dropbox': {
+                'display_name': 'Dropbox',
+                'details': 'A file backup, sync, and sharing solution used by more than 500 million users.',
+                'blackhole_path': 'Apps/QuietThyme/blackhole'
+
+            },
+            'skydrive': {
+                'display_name': 'OneDrive',
+                'details': 'Get to your files and photos from anywhere, on any device. Share and work together with anyone in your work and life.',
+                'blackhole_path': 'QuietThyme/blackhole'
+
+            },
+            'gdrive': {
+                'display_name': 'Google Drive',
+                'details': 'Get access to files anywhere through secure cloud storage and file backup for your photos, videos, files and more with Google Drive.',
+                'blackhole_path': 'QuietThyme/blackhole'
+            }
+        }
+    }
+
 }
--- src/app/partials/storage-panel/storage-panel.component.ts
+++ src/app/partials/storage-panel/storage-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {StorageStatus} from '../../models/storage-status'
+import {AppSettings} from '../../app-settings';
+
 @Component({
   selector: 'quietthyme-storage-panel',
   templateUrl: './storage-panel.component.html',
@@ -8,9 +10,12 @@ import {StorageStatus} from '../../models/storage-status'
 export class StoragePanelComponent implements OnInit {
   @Input() storageData : StorageStatus;
 
+    StorageDetails: any;
+
   constructor() { }
 
   ngOnInit() {
     console.log(this.storageData)
+      this.StorageDetails = AppSettings.STORAGE_DETAILS
   }
 }
