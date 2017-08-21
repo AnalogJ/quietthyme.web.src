19ef75e9796b93f3d4128354720dabc082844d20
Jason Kulatunga <jk17@ualberta.ca>
2017-08-20 23:28:40 -0700

    add Uservoice.

--- src/app/account-register-plan/account-register-plan.component.ts
+++ src/app/account-register-plan/account-register-plan.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
 import { ApiService } from '../services/api.service';
 import { ActivatedRoute } from '@angular/router';
+declare var UserVoice:any;
 
 @Component({
   selector: 'app-account-register-plan',
@@ -30,6 +31,23 @@ export class AccountRegisterPlanComponent implements OnInit {
             data => {
               //TODO: we should update the Token here too.
               console.log(data)
+
+              var tokenPayload = this.apiService.tokenPayload()
+              UserVoice.push(['identify', {
+                email:      tokenPayload.email, // User’s email address
+                name:       tokenPayload.name, // User’s real name
+                //created_at: 1364406966, // Unix timestamp for the date the user signed up
+                id:         tokenPayload.uid, // Optional: Unique id of the user (if set, this should not change)
+                type:       tokenPayload.plan, // Optional: segment your users by type
+                account: { // Account traits are only available on some plans
+                  // id:           123, // Optional: associate multiple users with a single account
+                  name:         tokenPayload.name, // Account name
+                  // created_at:   1364406966, // Unix timestamp for the date the account was created
+                  // monthly_rate: 9.99, // Decimal; monthly rate of the account
+                  // ltv:          1495.00, // Decimal; lifetime value of the account
+                  plan:         tokenPayload.plan // Plan name for the account
+                }
+              }]);
               //todo: redirect user to storage page
             },
             error => {console.log(error)},
--- src/app/account-register/account-register.component.ts
+++ src/app/account-register/account-register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
+declare var UserVoice:any;
 
 @Component({
   selector: 'app-account-register',
@@ -37,6 +38,16 @@ export class AccountRegisterComponent implements OnInit {
             data => {
               console.log(data)
               localStorage.setItem('id_token', data.token); //set the JWT token
+
+                var tokenPayload = this.apiService.tokenPayload()
+                UserVoice.push(['identify', {
+                    email:      tokenPayload.email, // User’s email address
+                    name:       tokenPayload.name, // User’s real name
+                    //created_at: 1364406966, // Unix timestamp for the date the user signed up
+                    id:         tokenPayload.catalog_token, // Optional: Unique id of the user (if set, this should not change)
+                    type:       'none' // Optional: segment your users by type
+                }]);
+
 
               this.router.navigate(['/register/plan'])
             },
