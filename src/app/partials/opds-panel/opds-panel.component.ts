fd94240e0103a266489677b10c908ad03ebe8016
Jason Kulatunga <jk17@ualberta.ca>
2017-08-21 17:24:09 -0700

    fix opds panel colors.

--- src/app/partials/opds-panel/opds-panel.component.ts
+++ src/app/partials/opds-panel/opds-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+declare var Granim:any;
 
 @Component({
   selector: 'quietthyme-opds-panel',
@@ -12,6 +13,23 @@ export class OpdsPanelComponent implements OnInit {
   constructor(private apiService: ApiService,) { }
   ngOnInit() {
     this.catalogUrl = this.apiService.catalogUrl()
+
+    var granimInstance = new Granim({
+      element: '#granim-opds',
+      name: 'granim',
+      opacity: [1, 1],
+      states : {
+        "default-state": {
+          gradients: [
+
+              //#4876BD,#5448BD,#8F48BD,#BD48B1
+            ['#4876BD', '#5448BD'],
+            // ['#8F48BD', '#BD48B1'],
+              ['#238b53', '#669e4b']
+          ]
+        }
+      }
+    });
   }
 
 }
